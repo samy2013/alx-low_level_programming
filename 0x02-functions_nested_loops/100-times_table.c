f917aee8e425123b902a0e0d4799766fdbcbbf23
samy2013 <samy.fawkey1@gmail.com>
2023-03-21 05:54:33 -0700

    Tasks of 0x02. C - Functions, nested loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,12 +1,13 @@
 #include "main.h"
 /**
- * print_times_table  prints the n times table-  starting with 0
+ * print_times_table - function that prints the n times table,
+ * starting with 0
+ *
  * @n: number of the times table
  */
 void print_times_table(int n)
 {
-		int i, j, k;
-
+int i, j, k;
 
 		if (n >= 0 && n <= 15)
 		{
